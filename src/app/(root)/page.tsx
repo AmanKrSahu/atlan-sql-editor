ad599f199a90a1b1dde358e2aa50a670106e501f
Aman <amankrsahu003@gmail.com>
2025-03-26 05:02:26 +0530

    feat(deps): added basic dependencies for the project

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from "./_css/page.module.css";
+
+export default function Home() {
+  return <div>Atlan Frontend Assignment</div>;
+}
